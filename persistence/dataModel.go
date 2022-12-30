e83ed6a2cdda37d043201258815f027e0682a27f
ehsan <ehsandavari.ir@gmail.com>
2022-12-30 20:13:10 +0330

    remove IModelConstraint

--- persistence/dataModel.go
+++ persistence/dataModel.go
@@ -2,13 +2,9 @@ package persistence
 
 import (
 	"golangCodeBase/domain/entities"
-	"golangCodeBase/infrastructure/postgres/models"
 )
 
 type (
-	IModelConstraint interface {
-		models.OrderModel
-	}
 	IDataModel[TE entities.IEntityConstraint] interface {
 		ToEntity() TE
 		FromEntity(entity TE) any
