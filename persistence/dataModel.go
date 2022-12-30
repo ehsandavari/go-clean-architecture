6215123fd02c6a04be2ff766cf0a5da932461487
ehsan <ehsandavari.ir@gmail.com>
2022-12-30 20:18:13 +0330

    cleanup

--- persistence/dataModel.go
+++ persistence/dataModel.go
@@ -4,9 +4,7 @@ import (
 	"golangCodeBase/domain/entities"
 )
 
-type (
-	IDataModel[TE entities.IEntityConstraint] interface {
-		ToEntity() TE
-		FromEntity(entity TE) any
-	}
-)
+type IDataModel[TE entities.IEntityConstraint] interface {
+	ToEntity() TE
+	FromEntity(entity TE) any
+}
